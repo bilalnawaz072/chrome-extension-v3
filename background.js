3f28467cef39ada513be2c6fc3e4229d18c3c8cc
Muhammad Bilal <bilalnawaz072@gmail.com>
2023-09-12 18:04:10 +0500

    Storage API

--- background.js
+++ background.js
@@ -20,13 +20,15 @@ chrome.contextMenus.create({
   
   // Send a message to the active tab's content script
   chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-    const message = {text: 'Hello from the background script!'};
-      await chrome.tabs.sendMessage(tab.id, message);
+    const message = {text: 'Hello from local storage!'};
+      // await chrome.tabs.sendMessage(tab.id, message);
+    // Save the message to local storage
+  await chrome.storage.local.set({message: message.text}, function() {
+    console.log('Message is set to local storage');
+  });
       console.log('message sent! From Background');
   });
   
-
-
 
 
 
--- content.js
+++ content.js
@@ -1,5 +1,12 @@
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message.text); // Log the message to the console
+  // Log the message to the console
+  console.log(message.text); 
+
   });
   
+  chrome.storage.local.get(['message'], function (result) {
+    console.log('Message is get  to local storage');
+    // Hello from the background script!
+    console.log('Message from local storage: ' + result.message);
+  });
